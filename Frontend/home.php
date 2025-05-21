e9ef608e83d5ae9f438f38e50aa507f3aaf389d0
GustavoDutra <gt.senacti@gmail.com>
2025-05-20 21:23:52 -0300

    Projeto_Front: Gustavo | Colocando projeto no Git

---
+++ Frontend/home.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="home.css">
+    <title>EixoAutopeças</title>
+</head>
+<body>
+    <header >
+        <div class="logo">
+            <img src="/EixoAuto/img/Logo E branca real.png" alt="">
+        </div>
+
+        <div class="header-content">
+            <div class="pesquisa">
+                <input placeholder="Buscar..." type="text">
+            </div>
+    
+            <nav>
+                <a href="">Sobre</a>
+                <a href="">Contato</a>
+                <a href="">Login</a>
+                <a href="/EixoAuto/pages/cadastro.html">Cadastro</a>
+            </nav>
+        </div>
+
+        <img id="menu" src="/EixoAuto/img/menu-de-hamburguer.png" alt="">
+
+        <div class="icons">
+            <img class= "icons" src  ="/EixoAuto/img/carrinho branco .png" alt="">
+            <img class= "icons" src ="/EixoAuto/img/estrela vazada.png" alt="">
+        </div>
+    </header>
+
+    <div class ="container">
+        <div class="primeiro"></div>
+        <div class="primeiro"></div>
+        <div class="primeiro"></div>
+        <div class="primeiro"></div>
+    </div>
+
+    <div class="collum">
+        <div id="segundo"></div>
+    </div>
+
+    <div class="container">
+        <div class="terceiro"></div>
+        <div class="quarto"></div>
+    </div>
+
+    <div class="contain">
+        <div class="quarto"></div>
+        <div class="terceiro"></div>
+    </div>
+   
+    <div class="circle">
+        <div class="circulo"></div>
+        <div class="circulo"></div>
+        <div class="circulo"></div>
+    </div>
+    
+</body>
+</html>
